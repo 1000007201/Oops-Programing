5cba8e371ae7875c056cdb71640806eed4e0b0b8
1000007201 <nishant.sharma5504@gmail.com>
2022-02-08 00:13:29 +0530

    [Nishant] Add:Json Inventory Done

---
+++ InventoryManagement/InventoryManage.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class InventoryManage
+    {
+        public void InventoryRead(string filepath)
+        {
+            var json = File.ReadAllText(filepath);
+            var items = JsonConvert.DeserializeObject<List<Inventory>>(json);
+            Console.WriteLine("Name\tPrice\tWeight\tTotalPrice");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Name + "\t" + item.Price + "\t" + item.Weight + "\t" + item.Price * item.Weight);
+            }
+        }
+    }
+}
+
---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InventoryManagement;
+
+InventoryManage inventoryManage = new InventoryManage();
+inventoryManage.InventoryRead(@"E:\Assignment1\Oops-Programing\InventoryManagement\Input.json");
+
