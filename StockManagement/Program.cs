931961a510645051fa61ee487b869a955599dc7a
1000007201 <nishant.sharma5504@gmail.com>
2022-02-08 23:27:00 +0530

    [Nishant] Add:Stock Management Done

---
+++ StockManagement/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StockManagement;
+
+
+StockManage stockManage = new StockManage();
+stockManage.StockDetails(@"E:\Bridgelabz\Oops-Programing\StockManagement\InputStock.json");
---
+++ StockManagement/StockManage.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockManagement
+{
+    public class StockManage
+    {
+        List<Stocks> stocks = new List<Stocks>();
+        public void StockDetails(string filepath)
+        {
+            var json = File.ReadAllText(filepath);
+            var item = JsonConvert.DeserializeObject<List<Stocks>>(json);
+            Console.WriteLine("Name\t\tPrice\tQuantity\tMarketCap");
+            foreach (var data in item)
+            {
+                Console.WriteLine($"{data.Name}\t\t{data.Price}\t{data.Quantity}\t{data.Price*data.Quantity}");
+            }
+        }
+    }
+}
